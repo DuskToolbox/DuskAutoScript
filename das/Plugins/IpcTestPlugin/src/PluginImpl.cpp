b13e937d5ed326d176d56ffb0c35a112a4d0fe85
Dusk_NM02 <dusk.11th@outlook.com>
2026-02-19 08:21:18 +0800

    fix(plugin): 修复 IpcTestPlugin 编译错误
    fix(plugin): fix IpcTestPlugin compilation errors
    
    - 移除 TestInterfaceImpl 实现类
    - 更新 PluginImpl.cpp 以适配新的代码结构
    
    ～(∠・ω< )⌒★ glm-5 代表 Dusk 创建了这个提交

--- das/Plugins/IpcTestPlugin/src/PluginImpl.cpp
+++ das/Plugins/IpcTestPlugin/src/PluginImpl.cpp
@@ -1,13 +1,12 @@
+#include "das/DasTypes.hpp"
 #include <das/DasApi.h>
 #include <das/IDasBase.h>
 #include <das/Utils/StringUtils.h>
 #include <das/_autogen/idl/abi/IDasPluginPackage.h>
-#include <das/_autogen/idl/abi/IDasTestInterface.h>
 
 #define DAS_BUILD_SHARED
 
 #include "PluginImpl.h"
-#include "TestInterfaceImpl.h"
 
 #include <array>
 #include <stdexcept>
@@ -37,21 +36,9 @@ DasResult IpcTestPlugin::CreateFeatureInterface(
     size_t index,
     void** pp_out_interface)
 {
-    DAS_UTILS_CHECK_POINTER_FOR_PLUGIN(pp_out_interface);
-    switch (index)
-    {
-    case 0:
-    {
-        const auto p_result =
-            MakeDasPtr<TestInterface::IDasTestInterface, TestInterfaceImpl>();
-        *pp_out_interface = p_result.Get();
-        p_result->AddRef();
-        return DAS_S_OK;
-    }
-    default:
-        *pp_out_interface = nullptr;
-        return DAS_E_OUT_OF_RANGE;
-    }
+    std::ignore = index;
+    pp_out_interface = nullptr;
+    return DAS_E_NO_IMPLEMENTATION;
 }
 
 static std::atomic_int32_t g_ref_count;
