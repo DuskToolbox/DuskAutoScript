9ea5f1819bf81fd33009408063618ab12d01295e
Dusk_NM02 <dusk.11th@outlook.com>
2026-05-06 09:31:33 +0800

    chore(dascv-gpumat): 删除迁移后无用的 DasCVModuleEntry 文件
    chore(dascv-gpumat): remove DasCVModuleEntry files orphaned after InitializeDasCore migration
    
    InitializeDasCore 已迁移到 IDasBase.h/cpp，DasCVModuleEntry 不再需要。
    
    ～(∠・ω< )⌒★ GLM-5 代表 Dusk 创建了这个提交

--- das/Core/OcvWrapper/include/das/Core/OcvWrapper/DasCVModuleEntry.h
+++
@@ -1,8 +0,0 @@
-#ifndef DAS_CORE_OCVWRAPPER_DASCVMODULEENTRY_H
-#define DAS_CORE_OCVWRAPPER_DASCVMODULEENTRY_H
-
-// CV module entry points.
-// cv.cpu / cv.cuda service registration is now in
-// InitializeDasCore() (see include/das/IDasBase.h).
-
-#endif // DAS_CORE_OCVWRAPPER_DASCVMODULEENTRY_H
--- das/Core/OcvWrapper/src/DasCVModuleEntry.cpp
+++
@@ -1,3 +0,0 @@
-// CV module entry points.
-// cv.cpu / cv.cuda service registration moved to
-// InitializeDasCore() in das/Core/ForeignInterfaceHost/src/IDasBase.cpp
