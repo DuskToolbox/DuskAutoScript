94e1ff842ddd4a680fc7ffd7778d3e1a88ec5b8c
Dusk_NM02 <dusk.11th@outlook.com>
2026-05-08 18:00:53 +0800

    test(idasmemory-offset-view): cover stable read views
    - update IDasMemory IDL to offset-view surface for RED gate
    - cover offset-zero migration and stable offset views through public ABI

--- das/Core/Utils/test/IDasMemoryTest.cpp
+++ das/Core/Utils/test/IDasMemoryTest.cpp
@@ -21,44 +21,65 @@ TEST(IDasMemoryTest, GetBinaryBufferReturnsZeroCopyView)
     ASSERT_EQ(::CreateIDasMemory(16, memory.Put()), DAS_S_OK);
 
     DAS::DasPtr<DAS::ExportInterface::IDasBinaryBuffer> first_buffer;
-    ASSERT_EQ(memory->GetBinaryBuffer(first_buffer.Put()), DAS_S_OK);
+    ASSERT_EQ(memory->GetBinaryBuffer(0, first_buffer.Put()), DAS_S_OK);
     auto* first_data = GetData(first_buffer.Get());
     first_data[3] = 42;
 
     DAS::DasPtr<DAS::ExportInterface::IDasBinaryBuffer> second_buffer;
-    ASSERT_EQ(memory->GetBinaryBuffer(second_buffer.Put()), DAS_S_OK);
+    ASSERT_EQ(memory->GetBinaryBuffer(0, second_buffer.Put()), DAS_S_OK);
     auto* second_data = GetData(second_buffer.Get());
 
     EXPECT_EQ(second_data, first_data);
     EXPECT_EQ(second_data[3], 42);
 }
 
-TEST(IDasMemoryTest, GetBinaryBufferRespectsOffset)
+TEST(IDasMemoryTest, GetBinaryBufferOffsetZeroIsWholeStorageMigration)
+{
+    DAS::DasPtr<DAS::ExportInterface::IDasMemory> memory;
+    ASSERT_EQ(::CreateIDasMemory(16, memory.Put()), DAS_S_OK);
+
+    DAS::DasPtr<DAS::ExportInterface::IDasBinaryBuffer> buffer;
+    ASSERT_EQ(memory->GetBinaryBuffer(0, buffer.Put()), DAS_S_OK);
+    auto* data = GetData(buffer.Get());
+    data[15] = 42;
+
+    uint64_t size = 0;
+    ASSERT_EQ(buffer->GetSize(&size), DAS_S_OK);
+
+    EXPECT_EQ(size, 16);
+    EXPECT_EQ(data[15], 42);
+}
+
+TEST(IDasMemoryTest, GetBinaryBufferCreatesStableOffsetViews)
 {
     DAS::DasPtr<DAS::ExportInterface::IDasMemory> memory;
     ASSERT_EQ(::CreateIDasMemory(16, memory.Put()), DAS_S_OK);
 
     DAS::DasPtr<DAS::ExportInterface::IDasBinaryBuffer> base_buffer;
-    ASSERT_EQ(memory->GetBinaryBuffer(base_buffer.Put()), DAS_S_OK);
+    ASSERT_EQ(memory->GetBinaryBuffer(0, base_buffer.Put()), DAS_S_OK);
     auto* base_data = GetData(base_buffer.Get());
+    base_data[4] = 17;
+    base_data[8] = 42;
 
-    ASSERT_EQ(memory->SetOffset(4), DAS_S_OK);
+    DAS::DasPtr<DAS::ExportInterface::IDasBinaryBuffer> offset_four_buffer;
+    ASSERT_EQ(memory->GetBinaryBuffer(4, offset_four_buffer.Put()), DAS_S_OK);
+    auto* offset_four_data = GetData(offset_four_buffer.Get());
 
-    DAS::DasPtr<DAS::ExportInterface::IDasBinaryBuffer> offset_buffer;
-    ASSERT_EQ(memory->GetBinaryBuffer(offset_buffer.Put()), DAS_S_OK);
-    auto* offset_data = GetData(offset_buffer.Get());
+    DAS::DasPtr<DAS::ExportInterface::IDasBinaryBuffer> offset_eight_buffer;
+    ASSERT_EQ(memory->GetBinaryBuffer(8, offset_eight_buffer.Put()), DAS_S_OK);
+    auto* offset_eight_data = GetData(offset_eight_buffer.Get());
 
-    uint64_t offset_size = 0;
-    ASSERT_EQ(offset_buffer->GetSize(&offset_size), DAS_S_OK);
+    uint64_t offset_four_size = 0;
+    ASSERT_EQ(offset_four_buffer->GetSize(&offset_four_size), DAS_S_OK);
 
-    EXPECT_EQ(offset_data, base_data + 4);
-    EXPECT_EQ(offset_size, 12);
-}
+    uint64_t offset_eight_size = 0;
+    ASSERT_EQ(offset_eight_buffer->GetSize(&offset_eight_size), DAS_S_OK);
 
-TEST(IDasMemoryTest, SetOffsetRejectsNegativeOffset)
-{
-    DAS::DasPtr<DAS::ExportInterface::IDasMemory> memory;
-    ASSERT_EQ(::CreateIDasMemory(16, memory.Put()), DAS_S_OK);
+    EXPECT_EQ(offset_four_data, base_data + 4);
+    EXPECT_EQ(offset_four_data[0], 17);
+    EXPECT_EQ(offset_four_size, 12);
 
-    EXPECT_EQ(memory->SetOffset(-1), DAS_E_OUT_OF_RANGE);
+    EXPECT_EQ(offset_eight_data, base_data + 8);
+    EXPECT_EQ(offset_eight_data[0], 42);
+    EXPECT_EQ(offset_eight_size, 8);
 }
